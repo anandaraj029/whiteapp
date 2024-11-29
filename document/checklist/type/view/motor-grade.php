0b0dfd17dde84830d051ab61c624471665775dcd
Jayasrithiyagarajan <104126539+Jayasrithiyagarajan@users.noreply.github.com>
2024-11-29 19:50:28 +0530

    new changes in checklist

--- document/checklist/type/view/motor-grade.php
+++ document/checklist/type/view/motor-grade.php
@@ -10,7 +10,7 @@ include_once('./view-fetch.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wheel Loader Inspection Checklist</title>
+    <title>Motor Grade Inspection Checklist</title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
